5f32e6cd2c6a8f46e99f7d4a93eb586de222f58f
bqminh30 <buiquangminh30102001@gmail.com>
2023-10-05 17:23:30 +0700

    update server room, service

--- app/config/db.js
+++ app/config/db.js
@@ -2,37 +2,16 @@ const mysql = require("mysql2");
 
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
   port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DB_DATABASE_NAME,
+  
 });
 connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!", err);
+  if (err) {
+    console.log( err);
+  }
 });
-
-module.exports = connection;
-
-// const { Client } = require('pg');
-// var fs = require('fs');
-
-// const connection = new Client({
-//   user: process.env.DB_PG_USERNAME,
-//   // host: 'localhost',
-//   database: process.env.DB_PG_DATABASE_NAME,
-//   password: process.env.DB_PG_PASSWORD,
-//   port: process.env.DB_PG_PORT,
-//   // ssl  : {
-//   //   ca : {
-//   //     require: true,
-//   //     rejectUnauthorized: false
-//   //   }
-//   // }
-// })
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
 
 module.exports = connection;
