dfdd633d6b3991a8778c1c602f1673479eedbb96
bqminh30 <buiquangminh30102001@gmail.com>
2023-11-06 10:57:26 +0700

    fix connection nodejs

--- app/config/db.js
+++ app/config/db.js
@@ -1,24 +1,28 @@
+require('dotenv').config()
 const mysql = require("mysql2");
 
-var connection = mysql.createConnection({
-  // host: "45.32.111.92",
-  // port: "3306",
-  // user: "bqminh",
-  // password: "6yhs3pBMFAcdB4LL",
-  // database: "bqminh",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-connection.connect(function (err) {
-  if (err) {
-    console.log(err);
-  }
-});
+function connection() {
+  return new Promise((resolve, reject) => {
+    let _connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+
+    _connection.connect(function (err) {
+      if (err) {
+        reject(err); // Khi có lỗi kết nối, reject Promise
+      } else {
+        console.log("Connected to the database");
+        resolve(_connection); // Khi kết nối thành công, resolve Promise với kết nối
+      }
+    });
+  });
+}
 
 module.exports = connection;
