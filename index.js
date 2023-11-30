76e3c9eba86061e52dda5339a0d5eeb8dd9cad22
bqminh <64219602+bqminh30@users.noreply.github.com>
2023-11-30 22:48:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -224,6 +224,55 @@ app.get("/process", function (req, res) {
 app.get("/cancel", (req, res) => {
   res.status(200).json({ success: false });
 });
+
+app.post('/create-paypal-payment', async (req, res) => {
+  const paymentData = req.body; // Thông tin đặt phòng từ React Native app
+
+  console.log('runnn')
+  const create_payment_json = {
+    intent: 'sale',
+    payer: {
+      payment_method: 'paypal',
+    },
+    redirect_urls: {
+      return_url: 'http://return.url', // URL để PayPal redirect sau khi thanh toán thành công
+      cancel_url: 'http://cancel.url', // URL để PayPal redirect nếu người dùng huỷ thanh toán
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: 'Hotel Booking',
+              price: 100,
+              currency: 'USD', // Đổi sang đơn vị tiền tệ của bạn
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: 'USD', // Đổi sang đơn vị tiền tệ của bạn
+          total: 100,
+        },
+        description: 'Hotel Booking Description',
+      },
+    ],
+  };
+
+  try {
+    // Tạo thanh toán PayPal
+    paypal.payment.create(create_payment_json, function (error, payment) {
+      if (error) {
+        throw error;
+      } else {
+        // Trả về URL để thanh toán PayPal cho ứng dụng React Native
+        res.status(200).json({ approval_url: payment.links[1].href });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 // set port, listen for requests
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
